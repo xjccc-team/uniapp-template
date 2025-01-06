d9ee126da87c8a8e3796bca8a08bb503ff779a0f
xjccc <546534045@qq.com>
2025-01-06 16:13:01 +0800

    feat: device store

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { createPinia } from 'pinia'
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import 'uno.css'
 export function createApp() {
   const app = createSSRApp(App)
+  app.use(createPinia())
+  // 添加全局系统变量
+  const systemInfo = uni.getSystemInfoSync()
+  const menuButtonBoundingClientRect = uni.getMenuButtonBoundingClientRect()
+  app.config.globalProperties.$systemInfo = systemInfo
+  app.config.globalProperties.$menuButtonBoundingClientRect = menuButtonBoundingClientRect
   return {
     app
   }
