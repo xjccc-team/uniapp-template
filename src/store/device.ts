cf28c06f1942e0cd2a666dd285d140d6acee7321
xjccc <546534045@qq.com>
2025-01-07 10:07:50 +0800

    update: store device

--- src/store/device.ts
+++ src/store/device.ts
@@ -6,18 +6,12 @@ export const useDeviceStore = defineStore('deviceStore', () => {
     throw new Error('getCurrentInstance returned null')
   }
   const { appContext } = app
-  console.log(appContext.config.globalProperties)
   const { statusBarHeight, platform, safeArea = {} } = appContext.config.globalProperties.$systemInfo
   const { top, height, width } = appContext.config.globalProperties.$menuButtonBoundingClientRect
 
-  // header区域距离胶囊按钮的位置距离
-  const headerPadding = top - statusBarHeight
-
-  const headerBarHeight = ref(height + headerPadding * 2) // header高度
-
-  const seriesTypeNavHeight = 44
-
-  const customHeaderHeight = ref(statusBarHeight)
+  // 胶囊按钮的位置距离
+  const menuPadding = top - statusBarHeight
+  const menuBarHeight = ref(height + menuPadding * 2) // header高度
 
   const isIOS = platform === 'ios'
   const isAndroid = platform === 'android'
@@ -25,13 +19,11 @@ export const useDeviceStore = defineStore('deviceStore', () => {
   // 头条小程序按钮会在搜索框上，看着效果无违和感，快手小程序的胶囊按钮宽度包含反馈按钮
   const menuButtonWidth = ref(width + 20)
 
-  customHeaderHeight.value = statusBarHeight + headerBarHeight.value
-
   return {
+    top,
     statusBarHeight,
-    headerBarHeight,
-    customHeaderHeight,
-    seriesTypeNavHeight,
+    menuBarHeight,
+    menuPadding,
     menuButtonWidth,
     menuButtonBoundingWidth: width,
     isIOS,
