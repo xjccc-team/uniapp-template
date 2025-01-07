5aa55bae4a03e46823d9fa4c70e79dac7dd902d3
xjccc <546534045@qq.com>
2025-01-07 09:16:28 +0800

    chore: delete unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
+// 组件自动引入有问题，无法识别自动引入组件上得class
+// 嵌套自动引入的组件，无法渲染
+// import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -11,10 +13,10 @@ export default defineConfig(async () => {
     plugins: [
       uni(),
       UnoCss(),
-      Components({
-        dts: 'src/components.d.ts',
-        dirs: ['src/components']
-      }),
+      // Components({
+      //   dts: 'src/components.d.ts',
+      //   dirs: ['src/components']
+      // }),
       AutoImport({
         dts: 'src/auto-import.d.ts',
         imports: [
