6c399a1686e47c3ec5305e0286778aefa553b65d
xjccc <546534045@qq.com>
2025-05-13 16:48:57 +0800

    update: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
-import * as Pinia from 'pinia'
+import { createPinia } from 'pinia'
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import 'uno.css'
 export function createApp() {
   const app = createSSRApp(App)
-  app.use(Pinia.createPinia())
+  app.use(createPinia())
   // 添加全局系统变量
   const systemInfo = uni.getSystemInfoSync()
   const menuButtonBoundingClientRect = uni.getMenuButtonBoundingClientRect()
   app.config.globalProperties.$systemInfo = systemInfo
   app.config.globalProperties.$menuButtonBoundingClientRect = menuButtonBoundingClientRect
-  app.mount('#app')
   return {
-    app,
-    Pinia
+    app
   }
 }
